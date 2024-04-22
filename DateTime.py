a626540677cb5403480e70fc431e15d582affdbd
Krishnakumar <149860096+krishnakumar-ng@users.noreply.github.com>
2024-04-22 21:51:53 +0530

    Update DateTime.py

--- DateTime.py
+++ DateTime.py
@@ -9,8 +9,8 @@ class DateTime:
         "Epoch Current Time": dateTimeUtil.getEpochTime(),
         "Epoch Milliseconds": dateTimeUtil.getEpochInMilli(),
         "Epoch Nanoseconds": dateTimeUtil.getEpochInNano(),
-        "Epoch_Today_Start": dateTimeUtil.getEpochForStartOfTheDay(),
-        "Epoch_Today_End": dateTimeUtil.getEpochForEndOfTheDay(),
+        "Epoch_Start_Of_The_Day": dateTimeUtil.getEpochForStartOfTheDay(),
+        "Epoch_End_Of_The_Day": dateTimeUtil.getEpochForEndOfTheDay(),
         "Epoch_24_Hrs_Forward": dateTimeUtil.getEpochFor24HoursForward(),
         "Epoch_24_Hrs_Back": dateTimeUtil.getEpochFor24HoursBack(),
         "ISO Format": dateTimeUtil.getTimeInIso(),
@@ -30,7 +30,13 @@ class DateTime:
     if __name__ == "__main__":
         
         print("=====================================================================================")
-        input_time = input("Enter the input time in any format or Enter now for the current time: \n")
+        print("Available Input formats: \n"
+              "1. Enter now for the current time\n"
+              "2. Enter the input time in any format\n"
+              "3. Enter add X days/minutes/hours to add X to the current time\n"
+              "4. Enter subtract X days/minutes/hours to subtract X from the current time\n")
+        
+        input_time = input()
         
         time = DateTimeParser.parse_input(input_time)
 
