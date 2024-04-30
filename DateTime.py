40b9315033135efb3053954e9f4a4c20a4fe36a9
Krishnakumar <149860096+krishnakumar-ng@users.noreply.github.com>
2024-04-30 19:37:15 +0530

    Updated DateTime.py to get EET and PST times

--- DateTime.py
+++ DateTime.py
@@ -6,7 +6,7 @@ class DateTime:
 
     def printAllTime(dateTimeUtil):
         output = {
-        "Epoch Current Time": dateTimeUtil.getEpochTime(),
+        "Epoch": dateTimeUtil.getEpochTime(),
         "Epoch Milliseconds": dateTimeUtil.getEpochInMilli(),
         "Epoch Nanoseconds": dateTimeUtil.getEpochInNano(),
         "Epoch_Start_Of_The_Day": dateTimeUtil.getEpochForStartOfTheDay(),
@@ -18,11 +18,15 @@ class DateTime:
         "ISO Format in UTC": dateTimeUtil.getTimeInUtc(),
         "ISO Format in IST": dateTimeUtil.getTimeInIst(),
         "ISO Format in EST": dateTimeUtil.getTimeInEst(),
+        "ISO Format in PST": dateTimeUtil.getTimeInPst(),
+        "ISO Format in EET": dateTimeUtil.getTimeInEet(),
         "ISO Format with 'Z'": dateTimeUtil.getTimeInIsoWithZ(),
         "ISO Format with zone Offset": dateTimeUtil.getTimeInIsoWithZoneOffset(),
         "ISO Format in UTC with 'Z'": dateTimeUtil.getTimeInUtcWithZ(),
         "ISO Format in IST with 'Z'": dateTimeUtil.getTimeInIstWithZ(),
         "ISO Format in EST with 'Z'": dateTimeUtil.getTimeInEstWithZ(),
+        "ISO Format in PST with 'Z'":dateTimeUtil.getTimeInPstWithZ(),
+        "ISO Format in EET with 'Z'":dateTimeUtil.getTimeInEetWithZ(),
         "Week of the year": dateTimeUtil.getWeekOfTheYear()
         }
         print(json.dumps(output, indent=4))
@@ -33,8 +37,8 @@ class DateTime:
         print("Available Input formats: \n"
               "1. Enter now for the current time\n"
               "2. Enter the input time in any format\n"
-              "3. Enter add X days/minutes/hours to add X to the current time\n"
-              "4. Enter subtract X days/minutes/hours to subtract X from the current time\n")
+              "3. Enter add X mins/hours/days/weeks/yrs to add X to the current time\n"
+              "4. Enter subtract/sub X mins/hours/days/weeks/yrs to subtract X from the current time\n")
         
         input_time = input()
         
